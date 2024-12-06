7136bfd663f2afb847b5c787526c667b54ee7d15
PPablo02 <100854090+PPablo02@users.noreply.github.com>
2024-12-05 22:25:04 -0600

    Update app.py

--- app.py
+++ app.py
@@ -337,7 +337,7 @@ def calcular_rendimiento_volatilidad(pesos, retornos):
         return rendimiento, volatilidad
 
 # Calcular el rendimiento y volatilidad para una serie de portafolios aleatorios
-def frontera_eficiente(retornos, num_portafolios=100000):
+def frontera_eficiente(retornos, num_portafolios=10000):
     # Número de activos
     n = len(retornos.columns)
     
